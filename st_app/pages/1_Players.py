4c2b9f02d169f5eed6488ee27d7c9f286606c81e
Maryam-Dollet <maryam.dollet@gmail.com>
2024-01-15 21:55:23 +0100

    update

--- st_app/pages/1_Players.py
+++ st_app/pages/1_Players.py
@@ -1,5 +1,10 @@
 import streamlit as st
+from cache_func import load_datasets
 
 st.set_page_config(layout="wide")
 
 st.title("Etude des joueurs")
+
+events = load_datasets()
+
+st.dataframe(events)
