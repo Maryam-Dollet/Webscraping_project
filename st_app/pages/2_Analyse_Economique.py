fa7f43725e5061960435a3dd0437420f76e7f167
Maryam-Dollet <maryam.dollet@gmail.com>
2024-01-29 14:38:29 +0100

    add requirements

--- st_app/pages/2_Analyse_Economique.py
+++ st_app/pages/2_Analyse_Economique.py
@@ -1,3 +1,5 @@
 import streamlit as st
 
-st.title("GDP of participant country")
+st.title("Analyse Economique des Pays Hôtes")
+
+st.write("Dans cette partie, nous tentons de réaliser une étude du profit des pays qui ont organisés les Jeux Olympiques.")
