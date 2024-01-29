8cf849ffbfa06d6b8e5605a2b91defa6d2d20286
Maryam-Dollet <maryam.dollet@gmail.com>
2024-01-29 18:09:56 +0100

    update

--- st_app/pages/2_Analyse_Économique.py
+++ st_app/pages/2_Analyse_Économique.py
@@ -55,8 +55,10 @@ filtered.Year =  filtered.Year.astype(int)
 
 gap = filtered.iloc[0].Annee
 final_filtered = filtered[filtered["Year"].between(gap, gap + 7)]
+final_filtered["pct_change"] = final_filtered["GDP"].pct_change() * 100
+final_filtered = final_filtered.fillna(0)
 
-st.dataframe(final_filtered)
+st.dataframe(final_filtered.style.format({"Year": lambda x: "{:}".format(x), "Annee": lambda x: "{:}".format(x)}))
 
 fig = px.line(final_filtered, x="Year", y="GDP")
 
