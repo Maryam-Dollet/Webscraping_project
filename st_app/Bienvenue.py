5ecb6384889aeb3047c5f6a488d86984afc0f1e8
Guillaumedt <114755843+Guillaumedt@users.noreply.github.com>
2024-01-30 11:28:32 +0100

    essai bienvenue

--- st_app/Bienvenue.py
+++ st_app/Bienvenue.py
@@ -6,6 +6,20 @@ st.set_page_config(layout="wide")
 
 st.title("Jeux Olympiques une étude de leur croissance et leur effet sur le monde")
 
+st.markdown("#### Dans le contexte de l’organisation des Jeux Olympiques 2024, imminents, un retour sur l’impact de ces jeux sur les différent(e)s villes et pays s’impose. ")
+st.markdown("#### Nous pouvons étudier et percevoir ces changements à travers 3 piliers qui se trouvent être ceux du développement durable : ")
+st.markdown("#### l’impact économique, écologique et social de ces JOs.")
+
+st.markdown("#### Comment les Jeux Olympiques se sont-ils développés au cours du temps et quels impacts ces développements ont-ils eus sur les pays organisateurs ?")
+
+st.markdown("Afin de répondre à ces questions, nous avons utilisé des données provenant de plusieurs sources :")
+st.markdown("Les sports.infos, tableaux des médailles : https://www.les-sports.info/2021-tokyo-jeux-olympiques-d-ete-s16-c0-b0-j0-u323.html")
+st.markdown("l’OCDE, émissions de gaz à effet de serre : https://stats.oecd.org/Index.aspx?DataSetCode=air_ghg")
+st.markdown("The world Bank, évolution du PIB par pays : https://databank.worldbank.org/reports.aspx?source=2&series=NY.GDP.MKTP.CD&country#")
+st.markdown("Olympedia, informations sur les athlètes : https://www.olympedia.org/athletes/1")
+
+# -- A compléter avec les autres sites -- #
+
 df_city_positions = load_positions()
 
 # st.dataframe(df_city_positions)
